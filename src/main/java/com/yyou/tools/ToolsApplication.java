74776d1774dc0d5ca635f9ce041a7c4a2b34bffc
yyou <1475750554@qq.com>
2020-03-16 16:35:28 +0800

    新增：prometheus

--- src/main/java/com/yyou/tools/ToolsApplication.java
+++ src/main/java/com/yyou/tools/ToolsApplication.java
@@ -1,13 +1,18 @@
 package com.yyou.tools;
 
 import com.yyou.tools.mapper.UserMapper;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
 @MapperScan(basePackageClasses = {UserMapper.class})
 public class ToolsApplication {
 
