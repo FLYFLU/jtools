beebd15ea1a37a1a96be501182a03477f67e4e0c
yyou <1475750554@qq.com>
2020-03-18 13:54:36 +0800

    新增：druid连接池 #3

--- src/main/java/com/yyou/tools/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yyou/tools/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.yyou.tools.exception;
 
 import com.yyou.data.CustomResponse;
-import com.yyou.data.HttpMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
