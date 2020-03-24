a60f3e34cb64f48ce848eea1090fbcea3bbc7b22
yyou <1475750554@qq.com>
2020-03-24 11:16:22 +0800

    提交临时

--- src/main/java/com/yyou/tools/util/AccessValidatorAutoconfig.java
+++ src/main/java/com/yyou/tools/util/AccessValidatorAutoconfig.java
@@ -1,13 +1,17 @@
 package com.yyou.tools.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AccessValidatorAutoconfig {
 
+    @Value("${Access.ExpireMinutes:30}")
+    private int expireMinutes;
+
     @Bean
     public IAccessValidator getAccessValidator(){
-        return new DefaultAccessValidator();
+        return new DefaultAccessValidator(expireMinutes);
     }
 }
--- src/main/java/com/yyou/tools/util/DefaultAccessValidator.java
+++ src/main/java/com/yyou/tools/util/DefaultAccessValidator.java
@@ -2,17 +2,40 @@ package com.yyou.tools.util;
 
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 
 import java.util.Date;
 
 public class DefaultAccessValidator implements IAccessValidator {
 
+    private int expireMinutes;
+
+    public DefaultAccessValidator(int expireMinutes) {
+        this.expireMinutes = expireMinutes;
+    }
+
+    private Date getExpireData(){
+        Date expireDate = null;
+        if(expireMinutes > 0){
+            expireDate = new Date(System.currentTimeMillis()+60*expireMinutes);
+        }
+        return expireDate;
+    }
+
+    private boolean isTokenAccess(String token){
+        JWT.require(Algorithm)
+    }
+
     @Override
     public String getToken(long id,String password) {
         String idString = Long.toString(id);
-        Date expireDate = new Date(System.currentTimeMillis()+60*30);
-        return JWT.create().withExpiresAt(expireDate).withAudience(idString).sign(Algorithm.HMAC256(password));
+        JWTCreator.Builder builder = JWT.create().withAudience(idString);
+        Date expireDate = getExpireData();
+        if(expireDate!=null){
+            builder.withExpiresAt(expireDate);
+        }
+        return builder.sign(Algorithm.HMAC256(password));
     }
 
     @Override
