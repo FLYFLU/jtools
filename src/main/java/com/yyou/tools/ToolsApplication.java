c9e36abe64ad0281d2ddb5cc6525b56c7517a7c0
wangtingming <1475750554@qq.com>
2021-01-22 14:37:01 +0800

    修改：删除数据库链接

--- src/main/java/com/yyou/tools/ToolsApplication.java
+++ src/main/java/com/yyou/tools/ToolsApplication.java
@@ -1,14 +1,14 @@
 package com.yyou.tools;
 
 import com.yyou.tools.dao.UserDao;
-import org.mapstruct.MapperConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
 @EnableTransactionManagement
 @MapperScan(basePackageClasses = {UserDao.class})
